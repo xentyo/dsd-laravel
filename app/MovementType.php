5bafd2befd1244b0e39d60a635446ba7a9058dfe
Christian Camacho <xentyomail@gmail.com>
2018-10-10 20:32:42 -0700

    Fix issue with API endpoint in movement types

--- app/MovementType.php
+++ app/MovementType.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class MovementType extends Model
 {
     protected $primaryKey = 'name';
+    public $incrementing = false;
     protected $fillable = ['name', 'description'];
 }
