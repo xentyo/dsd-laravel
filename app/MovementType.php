9d702b57172ef5b3600d219bb3ed1199d0391a38
Christian Camacho <xentyomail@gmail.com>
2018-10-10 20:08:49 -0700

    Resolve seed issue with a models

--- app/MovementType.php
+++ app/MovementType.php
@@ -6,6 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class MovementType extends Model
 {
-    protected $primary = 'name';
+    protected $primaryKey = 'name';
     protected $fillable = ['name', 'description'];
 }
